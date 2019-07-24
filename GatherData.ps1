8cd69e26f60a14c1bb608bc4487cdd1c3843fb8a
Dwayne Sutherland <DwayneaSutherland@gmail.com>
2019-07-24 12:29:54 +0100

    Added if block to stop script for checking for audit data if logging is not enabled for the company

--- GatherData.ps1
+++ GatherData.ps1
@@ -2,8 +2,8 @@ param(
 
     [int]$days = 7, #Days worth of logs
     [string]$singleCompany = $False, # $True when running against only one company
-    [string]$logPath = "$PSScriptRoot\Logs\GatherData-Log - $date.txt",
-    [string]$dataPath = "$PSScriptRoot\Raw Data\auditdata-$($date)-logins.csv"
+    [string]$logPath = "$PSScriptRoot\Logs\GatherData-Log - $(Get-Date -Format dd-MM-yy).txt",
+    [string]$dataPath = "$PSScriptRoot\Raw Data\auditdata-$(Get-Date -Format dd-MM-yy)-logins.csv"
 )
 
 function write-log {param([string]$message)
@@ -20,8 +20,6 @@ $accounts = import-csv -path "$PSScriptRoot\Accounts.csv"
 $retryMax = 3
 $retrySleepMinutes = 2
 
-# Date for file names
-$date = (Get-Date -Format dd-MM-yy)
 
 $auditData = @()
 
@@ -43,6 +41,7 @@ if ($singleCompany -eq $true) {
 }
 
 $accounts | ForEach-Object {
+    $auditingEnabled = $true
     $companyName = $_.name
     write-log -message "############## $companyName ##############"
 
@@ -54,89 +53,90 @@ $accounts | ForEach-Object {
         -Credential $cred -Authentication Basic -AllowRedirection    
     Import-PSSession $session 
 
+    
+
     $AuditLogEnabled = (Get-AdminAuditLogConfig).UnifiedAuditLogIngestionEnabled 
-    if ($AuditLogEnabled -ne $true) { 
+    if ($AuditLogEnabled -ne $true) {
+        Enable-OrganizationCustomization 
         write-log -message "Attempting to enable auditing" 
         try { Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled $true -erroraction stop }
         catch { write-log -message "Enabling Auditing failed" } 
+        $auditingEnabled = $False
     }
-
-    $LogSearchParams = @{
-        enddate        = $(get-date)
-        StartDate      = $((get-date).AddDays(-$days))
-        ResultSize     = 1000
-        sessionID      = $([DateTime]::Now.ToString().Replace('/', '_')).ToString
-        sessionCommand = 'ReturnNextPreviewPage'
-        RecordType     = 'AzureActiveDirectoryStsLogon'
-        erroraction    = 'stop'
-    }
-
-    $retryCount = 0  # Create retry counter
-    while ($true) {
-       
-        try {
-            [array]$results = Search-UnifiedAuditLog @LogSearchParams }
-        catch { write-log -message "Unable to search audit data" }
-        
-        Start-Sleep -Seconds 10
-
-        # if results contains data... 
-        if ( $results -ne $null -or $results.Count -ne 0 ) {   
-            $auditData += $results
-            $results = $null
-            Write-log -message "Audit Data found"    
-            continue 
+    
+    if($auditingEnabled -eq $true){
+        $LogSearchParams = @{
+            enddate        = $(get-date)
+            StartDate      = $((get-date).AddDays(-$days))
+            ResultSize     = 1000
+            sessionID      = $([DateTime]::Now.ToString().Replace('/', '_')).ToString
+            sessionCommand = 'ReturnNextPreviewPage'
+            RecordType     = 'AzureActiveDirectoryStsLogon'
+            erroraction    = 'stop'
         }
-        # if no data found within results...
-        if ($results -eq $null -or $results.Count -eq 0 ) {
-        
-            write-log -message "No data found begining sleep"     
-            Start-Sleep -Seconds $(60 * $retrySleepMinutes) 
+    
+        $retryCount = 0  # Create retry counter
+        while ($true) {
+            try { [array]$results = Search-UnifiedAuditLog @LogSearchParams }
+            catch { write-log -message "Unable to search audit data" }   
+            Start-Sleep -Seconds 10
 
-            # if results contains data after sleep... 
-            if ( $results -ne $null -or $results.Count -ne 0 ) {
-                $auditData += $results # Store data in auditData array
+            # if results contains data... 
+            if ( $results -ne $null -or $results.Count -ne 0 ) {   
+                $auditData += $results
                 $results = $null
-                write-log -message "Audit Data found" 
-                continue # search for more data
+                Write-log -message "Audit Data found"    
+                continue 
             }
-            else {
-                $retryCount ++ # Increment retry count
+            # if no data found within results...
+            if ($results -eq $null -or $results.Count -eq 0 ) {            
+                write-log -message "No data found begining sleep"     
+                Start-Sleep -Seconds $(60 * $retrySleepMinutes) 
+    
+                # if results contains data after sleep... 
+                if ( $results -ne $null -or $results.Count -ne 0 ) {
+                    $auditData += $results # Store data in auditData array
+                    $results = $null
+                    write-log -message "Audit Data found" 
+                    continue # search for more data
+                }
+                else {
+                    $retryCount ++ # Increment retry count
+                }
             }
+            # max amount of retries exceeded stop searching...
+            if ($retryCount -gt $retryMax -or $retryCount -eq $retryMax ) {   
+                write-log -message "Search complete"
+                # Stop searching
+                break 
+            }           
         }
-        # max amount of retries exceeded stop searching...
-        if ($retryCount -gt $retryMax -or $retryCount -eq $retryMax ) {   
-            write-log -message "Search complete"
-            # Stop searching
-            break 
-        }           
-    }
-    
-    write-log -message "Converting and sorting data from json"
-    $logArray = @()
+        
+        write-log -message "Converting and sorting data from json"
+        $logArray = @()
+        # Sort data and convert auditData from Json
+        $auditData | Sort-Object -Property UserIds | ForEach-Object{$_.auditdata} | ConvertFrom-Json | Select-Object -Property UserID,Operation,CreationTime,ClientIP `
+        |Where-Object Operation -eq 'UserLoggedIn'`
+        |ForEach-Object { 
+            $userLog = [PSCustomObject]@{
+                Company = $companyName
+                User = $_.userID
+                Operation = $_.Operation
+                CreationTime = $_.CreationTime
+                ClientIP = $_.ClientIP
+            }
 
-    # Sort data and convert auditData from Json
-    $auditData | sort -Property UserIds | %{$_.auditdata} | ConvertFrom-Json | select -Property UserID,Operation,CreationTime,ClientIP `
-    |Where-Object Operation -eq 'UserLoggedIn'`
-    |ForEach-Object { 
+            $logArray += $userLog
+        }
 
-        $userLog = [PSCustomObject]@{
-            Company = $companyNameue
-            User = $_.userID
-            Operation = $_.Operation
-            CreationTime = $_.CreationTime
-            ClientIP = $_.ClientIP
-        }     
-        $logArray += $userLog
+        write-log -message "convert and sort complete"
+        # add data for each company to a complete array
+        $completeData += $logArray 
     }
-    write-log -message "convert and sort complete"
-    # add data for each company to a complete array
-    $completeData += $logArray 
-    
     write-log -message "closing current company connection"
     Get-PSSession | Remove-PSSession 
 }
 write-log -message "All companies processed, exporting data"
 $completeData | Export-Csv -Path $dataPath
 
-
+Invoke-Command .\UpdateSql.ps1 -dataPath $dataPath
